4887546be6e3968af2f7b4a2156fd943a359e1ac
Silbye <silverxlxl9@gmail.com>
2023-11-24 19:23:05 +0100

    lesson5

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ product.name }}</h1>
+    <p>{{ formattedPrice }}</p>
+    <p>{{ checkAvailable }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      product: {
+        name: "Some goods",
+        price: "99.99",
+        available: true,
+      },
+    };
+  },
+  computed: {
+    formattedPrice() {
+      return `$${this.product.price}`;
+    },
+    checkAvailable() {
+      return this.product.available ? "Available" : "Out of stock";
+    },
+  },
+};
+</script>
